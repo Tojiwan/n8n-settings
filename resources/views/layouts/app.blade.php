aa79b4650d29adf74d72e171462cab63495eb96f
Tojiwan <hithug9909@gmail.com>
2025-09-01 14:41:52 +0800

    added layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Businesses</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+  <main class="d-flex align-items-center justify-content-center bg-dark vh-100">
+    @yield('content')
+  </main>
+</body>
+</html>
